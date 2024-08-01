87b28619d1285a46f519c8e4b84fc6b96fa58ddc
Peter St. John <pstjohn@nvidia.com>
2024-08-01 14:16:13 -0600

    Update devcontainer for new package structure (#62)

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,5 +2,9 @@
 
 pip install --disable-pip-version-check --no-cache-dir -e 3rdparty/Megatron-LM
 pip install --disable-pip-version-check --no-cache-dir -e 3rdparty/NeMo[all]
+pip install --disable-pip-version-check --no-cache-dir -e sub-packages/bionemo-core
+pip install --disable-pip-version-check --no-cache-dir -e sub-packages/bionemo-esm2
 pip install --disable-pip-version-check --no-cache-dir -e sub-packages/bionemo-fw
-pip install --disable-pip-version-check --no-cache-dir -e sub-packages/bionemo-contrib
+pip install --disable-pip-version-check --no-cache-dir -e sub-packages/bionemo-geneformer
+pip install --disable-pip-version-check --no-cache-dir -e sub-packages/bionemo-llm
+pip install --disable-pip-version-check --no-cache-dir -e sub-packages/bionemo-testing
