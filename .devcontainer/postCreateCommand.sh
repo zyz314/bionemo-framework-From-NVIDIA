1ca202bf8f710779da1d6b3dc88e44fc79d4abe1
Peter St. John <pstjohn@nvidia.com>
2024-09-11 10:37:11 -0600

    fix post-create command (#152)

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,8 @@
 
 pip install --disable-pip-version-check --no-cache-dir -e 3rdparty/Megatron-LM
 pip install --disable-pip-version-check --no-cache-dir -e 3rdparty/NeMo[all]
+
 for SUB_PKG in sub-packages/bionemo-*;
 do
-    pip install --disable-pip-version-check --no-cache-dir -e $SUB_PKG
+    pip install --disable-pip-version-check --no-cache-dir --no-deps -e $SUB_PKG
 done
